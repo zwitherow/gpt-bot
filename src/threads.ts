ed4aa199e1cb7fb685eeaa69b8beab131f0fa7c7
Zach Witherow <zwitherow@gmail.com>
2023-01-28 13:05:36 -0600

    show threads

--- src/threads.ts
+++ src/threads.ts
@@ -1,42 +1,47 @@
 import { v4 as uuid } from 'uuid'
 
 class Threads {
-  private threads: Thread[] = []
+  private _threads: Thread[] = []
 
-  private prune(): void {
+  private _prune(): void {
     const now = Date.now()
-    this.threads = this.threads.filter(
+    this._threads = this._threads.filter(
       thread => now - thread.time < 1000 * 60 * 60 * 24
     )
   }
 
+  threads(): Thread[] {
+    this._prune()
+    return this._threads
+  }
+
   getThreadByMessageId(messageId: string): Thread | undefined {
-    return this.threads.find(thread => thread.last === messageId)
+    return this._threads.find(thread => thread.last === messageId)
   }
 
   new(initialMessageId: string, prompt: string): Thread {
     const threadId = uuid()
-    this.threads.push({
+    this._threads.push({
       id: threadId,
       last: initialMessageId,
       time: Date.now(),
       conversation: [prompt]
     })
-    this.prune()
-    return this.threads.find(thread => thread.id === threadId)!
+    this._prune()
+    return this._threads.find(thread => thread.id === threadId)!
   }
 
   setLast(threadId: string, newLast: string, prompt: string): void {
-    const thread = this.threads.find(thread => thread.id === threadId)
+    const thread = this._threads.find(thread => thread.id === threadId)
     if (!thread) return
     thread.last = newLast
     thread.time = Date.now()
     thread.conversation.push(prompt)
-    this.prune()
+    this._prune()
   }
 
   prompt(threadId: string): string {
-    const thread = this.threads.find(thread => thread.id === threadId)
+    const thread = this._threads.find(thread => thread.id === threadId)
     if (!thread) return null
     const newMessages = thread.conversation.map((message, index) => {
       if (index % 2 === 0) {
